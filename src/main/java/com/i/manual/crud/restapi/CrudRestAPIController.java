3dc13da51cb7495a73019d777c75fdff530dcc5a
김동오 <dongkimko@gmail.com>
2025-02-16 21:45:29 +0900

    REST API 업데이트

--- src/main/java/com/i/manual/crud/restapi/CrudRestAPIController.java
+++ src/main/java/com/i/manual/crud/restapi/CrudRestAPIController.java
@@ -1,7 +1,12 @@
 package com.i.manual.crud.restapi;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.i.manual.crud.basic.CrudBasicDTO;
@@ -17,11 +22,45 @@ public class CrudRestAPIController {
     public CrudRestAPIController(CrudBasicService crudBasicService) {this.crudBasicService = crudBasicService;}
     
 	@GetMapping("/Crud/RestAPI/List")
-	public List<CrudBasicDTO> List() {
-		
+	public ResponseEntity<List<CrudBasicDTO>> List() {
+
         List<CrudBasicDTO> list = crudBasicService.list();
         
-        return list;
+        return ResponseEntity.ok(list);
     }
+		
+	@PostMapping("/Crud/RestAPI/Create")
+	public ResponseEntity<String> Create(@RequestBody CrudBasicDTO crudBasicDTO) {
+		
+		crudBasicService.create(crudBasicDTO);
+		
+		return ResponseEntity.ok("Success");
+	}
+	
+	@PostMapping("/Crud/RestAPI/Update/{idx}")
+	public ResponseEntity<String> Update(@PathVariable("idx") String idx, @RequestBody CrudBasicDTO crudBasicDTO) {
+
+		crudBasicDTO.setIdx(idx);
+		crudBasicService.update(crudBasicDTO);
+		
+		return ResponseEntity.ok("Success");
+	}
+	
+	@GetMapping("/Crud/RestAPI/Delete/{idx}")
+	public ResponseEntity<String> Delete(@PathVariable("idx") String idx) {
+		
+		crudBasicService.delete(idx);
+		
+		return ResponseEntity.ok("Success");
+	}
+	
+	@GetMapping("/Crud/RestAPI/Read/{idx}")
+	public ResponseEntity<CrudBasicDTO> Read(@PathVariable("idx") String idx, Model model) {
+		
+		crudBasicService.updateHit(idx);
+		CrudBasicDTO crudBasicDTO = crudBasicService.read(idx);
+		
+		return ResponseEntity.ok(crudBasicDTO);
+	}
 
 }
